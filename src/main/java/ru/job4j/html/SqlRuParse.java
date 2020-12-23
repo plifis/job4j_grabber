fc00b9e5c590c02be2a7fc21a236e7871aacdcde
plifis <nikich85@inbox.ru>
2020-12-23 16:20:28 +0300

    2. Парсинг HTML страницы. [#330424]

---
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -0,0 +1,23 @@
+package ru.job4j.html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SqlRuParse {
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        for (Element td : row) {
+            Element href = td.child(0);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+        }
+        Elements els = doc.select("[style=text-align:center].altCol");
+        for (Element td : els) {
+            System.out.println(td.text());
+        }
+
+    }
+}
