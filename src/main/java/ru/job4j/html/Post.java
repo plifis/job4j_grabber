cf81d862188dc4154e4a3e90848fd568d4abce48
plifis <nikich85@inbox.ru>
2020-12-28 12:21:05 +0300

    2.2. Модель данных - Post. [#330429]

---
+++ src/main/java/ru/job4j/html/Post.java
@@ -0,0 +1,32 @@
+package ru.job4j.html;
+
+public class Post {
+    private String vacancy;
+    private String description;
+    private Integer salary;
+
+    public String getVacancy() {
+        return vacancy;
+    }
+
+    public void setVacancy(String vacancy) {
+        this.vacancy = vacancy;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+}
