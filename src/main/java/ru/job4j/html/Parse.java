78f1d5348b5e8899717886be546db9d1c0ee4bec
plifis <nikich85@inbox.ru>
2020-12-30 12:34:21 +0300

    3. Архитектура проекта - Аргегатора Java Вакансий [#330425]

--- src/main/java/ru/job4j/html/Parse.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.html;
-
-import java.util.List;
-
-public interface Parse {
-        List<Post> list(String link);
-        Post detail(String link);
-    }
-
--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,6 +4,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Parse;
+
 import java.io.IOException;
 import java.text.DateFormatSymbols;
 import java.text.ParseException;
