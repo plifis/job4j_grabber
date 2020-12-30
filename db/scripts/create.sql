e277f20fb94edf8a87c3c437dc19be248bfd9438
plifis <nikich85@inbox.ru>
2020-12-30 14:54:49 +0300

    4. Схема таблицы Post [#330435]

---
+++ db/scripts/create.sql
@@ -0,0 +1,9 @@
+create database grabber;
+
+create table post(
+                     id serial primary key,
+                     name varchar(50),
+                     text text,
+                     link text,
+                     created timestamp
+);
