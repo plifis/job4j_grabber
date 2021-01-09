b5994b931e0fcaf633bf85fd5ec389265741088d
plifis <nikich85@inbox.ru>
2021-01-09 23:34:09 +0300

    0. Техническое задание. Аргегатора Java Вакансий. [#330423]

--- src/test/java/ru/job4j/html/SqlRuParseTest.java
+++ src/test/java/ru/job4j/html/SqlRuParseTest.java
@@ -2,14 +2,26 @@ package ru.job4j.html;
 
 import org.junit.Test;
 
+import java.text.ParseException;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
+
+import static org.junit.Assert.assertEquals;
 
 public class SqlRuParseTest {
 
     @Test
-    public void timeOfDay() {
-        Calendar calendar = new GregorianCalendar();
-        String strTime = "12:30";
+    public void convertDateFromString()  {
+        Calendar calendar = new GregorianCalendar(2020, Calendar.DECEMBER, 22, 8, 22);
+        String str = "22 дек 20, 8:22";
+        SqlRuParse sqlRuParse = new SqlRuParse();
+        Date actual = null;
+        try {
+            actual = sqlRuParse.convertDate(str);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        assertEquals(calendar.getTime(), actual);
     }
 }
